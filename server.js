524810d8fad49a800883e71ac3d9a859742efe18
CJL Fortunato <clemence.fortunato@gmail.com>
2023-06-09 09:36:22 +0200

    Create project

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import {
+  router
+} from './app/router.js';
+
+const app = express();
+app.use(express.json());
+app.use('/', router)
+
+app.listen(5000, () => console.log('Listening on port 5000...'));

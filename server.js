1e946e9163a767db4f33a84c3ef6f646c17c7ea7
Mitchrom <romaricmitch@gmail.com>
2023-06-09 10:53:24 -0300

    Ajout condition de vérification pour la requête post + requête delete

--- server.js
+++ server.js
@@ -20,9 +20,18 @@ app.get("/books/:id", (req, res) => {
 
 //post
 app.post("/books", (req, res) => {
+  //ajouter condition !== id déjà présents dans library.json
+  //prendre tous les id de library.json pour vérification
+  let fileId = [];
+  data.map((ele) => (fileId = [...fileId, ele.id]));
+  // console.log(fileId);
+
   const { body } = req;
-  data = [...data, body];
-  console.log(data);
+  ///vérification
+  if (!fileId.includes(body.id)) data = [...data, body];
+  else res.send(`L'id ${body.id} est déjà présent dans la bibliothèque`);
+
+  //si vérification réussie, insertion de la nouvelle data dans library.json
   try {
     fs.writeFileSync("library.json", JSON.stringify(data), "utf8");
     console.log("nouveau livre ajouté à la bibliothèque");
@@ -30,6 +39,32 @@ app.post("/books", (req, res) => {
     console.log("erreur lors de l'ajout ", error);
   }
   res.send("Succès de l'ajout du nouveau livre: " + JSON.stringify(body));
+});
+
+//delete
+app.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+  let dataAfterDelete = [];
+  let fileId = [];
+  data.map((ele) => (fileId = [...fileId, ele.id]));
+  console.log(fileId);
+
+  if (fileId.includes(parseInt(id))) {
+    dataAfterDelete = data.filter((ele) => ele.id !== parseInt(id));
+    console.log("après opération", dataAfterDelete);
+    try {
+      fs.writeFileSync("library.json", JSON.stringify(dataAfterDelete), "utf8");
+      console.log(`Le livre possédant l'id ${id} a été supprimé`);
+    } catch (error) {
+      console.log("erreur lors de la suppression ", error);
+    }
+  } else {
+    res.send(
+      "Veuillez choisir un ID enregistré dans la bibliothèque pour supprimer un livre"
+    );
+  }
+
+  res.send("Livre effacé");
 });
 
 app.listen(3000, () => console.log("Server is running"));

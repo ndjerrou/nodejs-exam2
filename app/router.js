ca1a6cc7a70608e35b604b5e13d21525c9769f81
bastien malgouyres <bmalgouyres@free.fr>
2023-06-09 15:58:44 +0200

    add comments

--- app/router.js
+++ app/router.js
@@ -16,6 +16,8 @@ export const router = express.Router();
 router.route('/:id').get(getBook);
 //GET localhost:3000/books/:id
 
+//return book or undefined
+
 router.route('/:id').put(validateBookChangeload, putBook);
 //PUT localhost:3000/books/:id
 /*
@@ -25,18 +27,29 @@ router.route('/:id').put(validateBookChangeload, putBook);
    "nationality?": "FR" -- option
 }
 */
+//return changed book or undefined
 
 router.route('/:id').delete(deleteBook);
 //DELETE localhost:3000/books/:id
+
+// return "delete" if there is a deletion or undefined 
 
 router.route('').get(getBooks);
 //GET localhost:3000/books?author=author3&page=0
 // author & page optional
 
+// return array of books, up to 5 if querry page 
+
 router.route('').post(validateBookload, postBook);
 //POST localhost:3000/books
 /*
 {
+   "title": "aa book",
+   "author": "author3",
+   "nationality": "FR"
+}
+*/
+/* return posted book json {
    "title": "aa book",
    "author": "author3",
    "nationality": "FR"
